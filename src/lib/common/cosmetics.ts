10b305f648c4d00c274071d141e4f8a381137de7
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2023-07-06 21:37:53 +0800

    src(cosmetics): export each category individually instead

--- src/lib/common/cosmetics.ts
+++ src/lib/common/cosmetics.ts
@@ -1,9 +1,51 @@
-export const cosmetics = {
-    hat: [{
-        name: 'Default Glasses',
-        price: 0,
-        description: 'Default glasses for your doobie.',
-    }],
-    glasses: [],
-    clothes: [],
+export const hats: Cosmetic[] = [
+	{
+		name: 'Default Glasses',
+		price: 0,
+		description: 'Default glasses for your pet doobie.',
+        path: 'images/glasses/default.png',
+	}
+];
+
+export const eyewears: Cosmetic[] = [];
+
+export const outfits: Cosmetic[] = [];
+
+export const sets: Cosmetic[] = [
+	{
+		name: 'Easter Set',
+		price: 3000,
+		description: 'Easter set for your pet doobie.',
+		path: 'images/sets/easter.png',
+		exclusive: false
+	},
+	{
+		name: 'Christmas Set',
+		price: 3000,
+		description: 'Christmas set for your pet doobie.',
+		path: 'images/sets/christmas.png',
+		exclusive: false
+	},
+	{
+		name: 'Mexican Set',
+		price: 3000,
+		description: 'Mexican set for your pet doobie.',
+		path: 'images/sets/mexican.png',
+		exclusive: false
+	},
+	{
+		name: 'Summer Set',
+		price: 3000,
+		description: 'Summer set for your pet doobie.',
+		path: 'images/sets/summer.png',
+		exclusive: false
+	}
+];
+
+export interface Cosmetic {
+    name?: string;
+    price?: number;
+    description?: string;
+    path?: string;
+    exclusive?: boolean;
 }
