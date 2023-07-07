be16b4596f3d5aeb720c63daf4552d057d150164
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2023-07-07 21:16:09 +0800

    misc(constants): add 8ball answers

--- src/lib/common/constants.ts
+++ src/lib/common/constants.ts
@@ -5,3 +5,28 @@ export const srcDir = join(rootDir, 'src');
 export const assetDir = join(rootDir, 'assets');
 
 export const RandomLoadingMessage = ['Computing...', 'Thinking...', 'Cooking some food', 'Give me a moment', 'Loading...'];
+
+export const eightBallAnswers = [
+    'It is certain.',
+    'It is decidedly so.',
+    'Without a doubt.',
+    'Yes - definitely.',
+    'You may rely on it.',
+    'As I see it, yes.',
+    'Most likely.',
+    'Outlook good.',
+    'Yes.',
+    'Signs point to yes.',
+    'Reply hazy, try again.',
+    'Ask again later.',
+    'Better not tell you now...',
+    'No',
+    'My sources say no.',
+    'Outlook not so good...',
+    'Very doubtful.',
+    'Maybe',
+    'I don\'t know',
+    'I don\'t think so',
+    'I don\'t know, maybe',
+    'I don\'t know, maybe not'
+];
