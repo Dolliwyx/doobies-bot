83444df4a99b822bb11059a66e97cd98c7853ac6
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2023-07-07 21:46:48 +0800

    feat(listeners): 8ballQuestion

---
+++ src/listeners/events/8ballQuestion.ts
@@ -0,0 +1,18 @@
+import { Events, Listener, container } from '@sapphire/framework';
+import type { Message } from 'discord.js';
+import { eightBallAnswers } from '../../lib/common/constants';
+import { ApplyOptions } from '@sapphire/decorators';
+
+const regexPrefix = container.client.options.regexPrefix!;
+
+@ApplyOptions<Listener.Options>({
+	event: Events.MessageCreate
+})
+export class UserEvent extends Listener<typeof Events.MessageCreate> {
+	public override async run(message: Message) {
+		console.log(message.content);
+		if (!regexPrefix.test(message.content)) return;
+		if (!message.content.endsWith('?')) return;
+		return message.reply(eightBallAnswers[Math.floor(Math.random() * eightBallAnswers.length)]);
+	}
+}
