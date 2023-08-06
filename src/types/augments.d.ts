6fe2d889bdba909f6811c740126f9c5f45f29db7
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2023-08-06 16:02:08 +0800

    misc: Update augments

--- src/types/augments.d.ts
+++ src/types/augments.d.ts
@@ -1,14 +1,23 @@
-import type { ArrayString } from "@skyra/env-utilities";
-import type { Settings } from "../lib/structures/Settings";
+import type { ArrayString } from '@skyra/env-utilities';
+import type { Settings } from '../lib/structures/Settings';
+import type { Shoukaku } from 'shoukaku';
+import type { QueueManager } from '../lib/structures/QueueManager';
 
 declare module '@skyra/env-utilities' {
 	interface Env {
 		OWNERS: ArrayString;
+		DISCORD_TOKEN: string;
+		DATABASE_URL: string;
+		SPOTIFY_CLIENTID: string;
+		SPOTIFY_SECRET: string;
+		LAVALINK_URL: string;
+		LAVALINK_PORT: string;
+		LAVALINK_AUTH: string;
 	}
 }
 
 declare module '@sapphire/pieces' {
-    interface Container {
-        settings: Settings;
-    }
+	interface Container {
+		settings: Settings;
+	}
 }
