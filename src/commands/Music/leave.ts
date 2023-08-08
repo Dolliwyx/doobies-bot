4d884e5fa95b8a2c5e4239819e1eb50069c00379
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2023-08-08 21:16:30 +0800

    feat(Music): leave command

---
+++ src/commands/Music/leave.ts
@@ -0,0 +1,23 @@
+import { DJOnly } from '#lib/decorators/DJOnly';
+import { ApplyOptions } from '@sapphire/decorators';
+import { Command } from '@sapphire/framework';
+
+@ApplyOptions<Command.Options>({
+	description: 'Leave the voice channel',
+	preconditions: ['GuildVoiceOnly', 'SameVC']
+})
+export class UserCommand extends Command {
+	public override registerApplicationCommands(registry: Command.Registry) {
+		registry.registerChatInputCommand((builder) => builder.setName(this.name).setDescription(this.description));
+	}
+
+	@DJOnly()
+	public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+		const guildPlayer = this.container.queue.get(interaction.guildId!)!;
+		if (!guildPlayer) return interaction.reply({ content: 'There is no music playing in this server.', ephemeral: true });
+
+		await guildPlayer.disconnect();
+
+		return interaction.reply({ content: 'Left the voice channel.' });
+	}
+}
