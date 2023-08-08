0da23e9a78189212c0b2284c88a348fd67500e3a
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2023-08-08 21:41:26 +0800

    fix(constants): fix asset directory path

--- src/lib/common/constants.ts
+++ src/lib/common/constants.ts
@@ -2,7 +2,7 @@ import { URL } from 'node:url';
 
 export const rootDir = new URL('../../../', import.meta.url);
 export const srcDir = new URL('src/', rootDir);
-export const assetDir = new URL('assets/', srcDir);
+export const assetDir = new URL('assets/', rootDir);
 
 export const RandomLoadingMessage = ['Computing...', 'Thinking...', 'Cooking some food', 'Give me a moment', 'Loading...'];
 
