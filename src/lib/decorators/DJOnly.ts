546c9247cbf3dd1e014f6377b7d4771f05c6d38a
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2023-08-08 16:25:16 +0800

    misc(DJOnly): change precondition to work in interactions instead of messages

--- src/lib/decorators/DJOnly.ts
+++ src/lib/decorators/DJOnly.ts
@@ -1,15 +1,15 @@
 import { createFunctionPrecondition } from '@sapphire/decorators';
 import { UserError, container } from '@sapphire/framework';
-import type { Message } from 'discord.js';
+import type { Interaction } from 'discord.js';
 
 export function DJOnly(): MethodDecorator {
-	return createFunctionPrecondition(async (message: Message) => {
-		const guildPlayer = container.queue.get(message.guild!.id);
-		if (!guildPlayer) return false;
+	return createFunctionPrecondition(async (interaction: Interaction) => {
+		const guildPlayer = container.queue.get(interaction.guildId!);
+		if (!guildPlayer) return true;
 		const {
 			music: { djs }
 		} = await container.settings.getUserSetting(guildPlayer.owner);
-		if (guildPlayer.owner !== message.author.id && !djs?.includes(message.author.id)) {
+		if (guildPlayer.owner !== interaction.user.id && !djs?.includes(interaction.user.id)) {
 			throw new UserError({ message: 'The owner of this music session does not have you as a DJ. Have them add you using `/dj add`!', identifier: 'PermissionsMissing' });
 		}
 		return true;
