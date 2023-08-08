8b7f796d5b66ac325464cf3c970ec30e0988c338
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2023-08-08 21:11:03 +0800

    fix(decorators): use correct enum string

--- src/lib/decorators/ModOnly.ts
+++ src/lib/decorators/ModOnly.ts
@@ -1,13 +1,13 @@
 import { createFunctionPrecondition } from '@sapphire/decorators';
 import { UserError } from '@sapphire/framework';
 import { envParseArray } from '@skyra/env-utilities';
-import type { Message } from 'discord.js';
+import { type Message } from 'discord.js';
 
 export function ModOnly(): MethodDecorator {
 	return createFunctionPrecondition(async (message: Message) => {
 		if (
 			!envParseArray('OWNERS').includes(message.author.id) ||
-			!message.member?.permissions.toArray().some((perms) => ['ADMINISTRATOR', 'MANAGE_GUILD', 'MODERATE_MEMBERS'].includes(perms))
+			!message.member?.permissions.toArray().some((perms) => ['Administrator', 'ManageGuild', 'ModerateMembers'].includes(perms))
 		) {
 			throw new UserError({ message: 'You must be an moderator to use this command.', identifier: 'PermissionsMissing' });
 		}
