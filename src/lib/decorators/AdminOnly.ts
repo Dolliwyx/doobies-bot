7ab52d539d8b089fe9c22dfd1bf0b5aaefef42f7
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2023-08-08 20:40:26 +0800

    fix(decorators): fix AdminOnly and ModOnly

--- src/lib/decorators/AdminOnly.ts
+++ src/lib/decorators/AdminOnly.ts
@@ -7,7 +7,7 @@ export function AdminOnly(): MethodDecorator {
 	return createFunctionPrecondition(async (message: Message) => {
 		if (
 			!envParseArray('OWNERS').includes(message.author.id) ||
-			message.member?.permissions.toArray().some((perms) => perms.includes('ADMINISTRATOR'))
+			!message.member?.permissions.toArray().some((perms) => perms.includes('ADMINISTRATOR'))
 		) {
 			throw new UserError({ message: 'You must be an administrator to use this command.', identifier: 'PermissionsMissing' });
 		}
--- src/lib/decorators/ModOnly.ts
+++ src/lib/decorators/ModOnly.ts
@@ -7,7 +7,7 @@ export function ModOnly(): MethodDecorator {
 	return createFunctionPrecondition(async (message: Message) => {
 		if (
 			!envParseArray('OWNERS').includes(message.author.id) ||
-			message.member?.permissions.toArray().some((perms) => ['ADMINISTRATOR', 'MANAGE_GUILD', 'MODERATE_MEMBERS'].includes(perms))
+			!message.member?.permissions.toArray().some((perms) => ['ADMINISTRATOR', 'MANAGE_GUILD', 'MODERATE_MEMBERS'].includes(perms))
 		) {
 			throw new UserError({ message: 'You must be an moderator to use this command.', identifier: 'PermissionsMissing' });
 		}
