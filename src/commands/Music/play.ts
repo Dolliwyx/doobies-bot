742cbbafe9b8ac25d1ef8656256f3e9d3a3758e2
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2023-08-29 21:38:06 +0800

    refactor: attempt to add a retry function when spotify api timeouts

--- src/commands/Music/play.ts
+++ src/commands/Music/play.ts
@@ -50,6 +50,15 @@ export class UserCommand extends Command {
 
 		const res = await node?.rest.resolve(query);
 
+		if (res?.loadType === LoadType.ERROR) {
+			const maxRetries = 3;
+			for (let retry = 0; retry < maxRetries; retry++) {
+				await interaction.editReply({ content: `Found no results, retrying... (${retry + 1}/${maxRetries})` });
+				const res = await node?.rest.resolve(query);
+				if (res || ![LoadType.ERROR, LoadType.EMPTY].includes(res!.loadType)) break;
+			}
+		}
+
 		if (!res || [LoadType.ERROR, LoadType.EMPTY].includes(res.loadType))
 			return interaction.editReply({ content: 'No results were found for your query.' });
 
