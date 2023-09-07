d770721c758500fb9493e07efed3c055488f86ba
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2023-09-07 15:51:56 +0800

    refactor: use a barrel for types

---
+++ src/lib/types/index.ts
@@ -0,0 +1,7 @@
+export * from './ConstructorStanding.js';
+export * from './DriverStanding.js';
+export * from './F1RaceResult.js';
+export * from './F1Schedule.js';
+export * from './GuildSettings.js';
+export * from './index.js';
+export * from './UserSettings.js';
